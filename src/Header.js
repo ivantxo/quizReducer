30f53de60b97f7aee9a71f59ec28c23a7fda317b
IvanR <ivan.ramirezf@gmail.com>
2024-01-16 04:48:35 +1000

    Added Header component

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div className="app-header">
+      <img src="logo.svg" alt="React logo" />
+      <h1>React Quiz</h1>
+    </div>
+  );
+}
+
+export default Header;
