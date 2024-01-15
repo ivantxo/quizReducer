67d84fd64f1bcf2e3243169a03b2d3fa7e42bc21
IvanR <ivan.ramirezf@gmail.com>
2024-01-16 05:54:30 +1000

    Reducer, useEffect, initialState
    - Added initialState
    - Added reducer
    - Fetching questions and loading into questions state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import StartScreen from "./StartScreen";
 
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highScore: 0,
+  secondsRemaining: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, status: "ready", questions: action.payload };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 function App() {
+  const [{ questions }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((error) => {
+        throw new Error("Error");
+      });
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <Main>
-        <StartScreen numQuestions={2} />
+        <StartScreen numQuestions={numQuestions} />
       </Main>
     </div>
   );
